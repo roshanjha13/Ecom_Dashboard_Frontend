97f92e52a31d7f95149cf4a590011ca881401302
Roshan <roshan.jha@hih7.in>
2023-09-12 16:23:40 +0530

    Add Router and Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import Nav from "./component/Header/Nav";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <h1>E-DashBoard</h1>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<h1>Product Listing Component </h1>} />
+          <Route path="/add" element={<h1> Add Product Component </h1>} />
+          <Route path="/update" element={<h1>update Product Component </h1>} />
+          <Route path="/logout" element={<h1> Logout Component </h1>} />
+          <Route path="/profile" element={<h1>Its ME</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
